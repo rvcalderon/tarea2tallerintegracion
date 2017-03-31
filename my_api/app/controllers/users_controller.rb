023142f5ef83c476fde187b4434b71a9d27ca7d6
Rosario Calderon <rvcalderon@uc.cl>
2017-03-31 15:10:52 -0300

    ultimos tests

--- my_api/app/controllers/users_controller.rb
+++ my_api/app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  require 'json'
+  
+
   def index
     @users = User.all
     message = {'usuarios' => @users, 'total' => @users.count}.to_json
@@ -7,35 +10,77 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: @user 
+    
+    if User.exists?(id:params[:id]) 
+      @user = User.find(params[:id])  
+      render json: @user
+    else
+      render json: {error: "Usuario no encontrado"}, status:404
+    end
   end
 
   def create
-    @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created
+    @body = JSON.parse request.body.read
+    @keys = @body.keys
+
+    if @keys.include?"id"
+      render json: {error: "No se puede crear usuario con id"}, status: 400
+    
     else
-      render json: @user.errors, status: :unprocessable_entity
+      @user = User.new(user_params)
+
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
   end
 
   def update
-    if @user.update(user_params)
-      head :no_content
+    
+
+    puts 'impresion:'
+    puts @keys
+
+    if User.exists?(id: params[:id]) 
+
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        render json: @user, status: :ok
+      else
+        msg = {'error' => 'La modificación ha fallado'}.to_json
+        render json: msg, status: :internal_server_error
+      end
+      
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: {error: "Usuario no encontrado"}, status:404
     end
   end
 
   def destroy
-    @user.destroy
+    if User.exists?(id:params[:id]) 
+      @user = User.find(params[:id])
+      @user.destroy
+    else
+      render json: {error: "Usuario no encontrado"}, status:404
+    end
 
-    head :no_content
+    
   end 
 
   def user_params
-      params.require(:user).permit(:usuario, :nombre, :apellido, :twitter)
+      params.permit(:usuario, :nombre, :apellido, :twitter)
+  end
+
+  def set_user
+     if User.exists?(id:params[:id])
+       @user=User.find(params[:id])
+    else
+       @user = nil
+    end
   end
 
 end
--- my_api/app/models/user.rb
+++ my_api/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	validates_presence_of :usuario
+	validates_presence_of :nombre
 end
